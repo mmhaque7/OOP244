51eabe374ffea9b55203651ed419f9529b8e2538
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2018-09-08 07:23:44 -0400

    removed 2184

--- WS04/in-lab/Passenger.h
+++
@@ -1,8 +0,0 @@
-// TODO: add file header comments here
-
-// TODO: add header file guard here
-
-// TODO: declare your namespace here
-
-    // TODO: define the Passenger class here
-
--- WS08/in_lab/SavingsAccount.cpp
+++
@@ -1,3 +0,0 @@
-
-#include "SavingsAccount.h"
-
